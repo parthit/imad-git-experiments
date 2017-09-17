bbf2ceddbc3fa82ed333c232f6c4b98a1570a835
Parthit <parthit@localhost.localdomain>
2017-09-17 20:27:56 +0530

    Bug fixed in something()

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 function something (){
 
 	console.log('I\'m something inn the something function');
+	console.log('Bug fixes were done over here, and now it is ready');
 }
 
 console.log('hello world');
