c9b2a21a5576a9cc0d816d79d4bceff50cedc0d4
Parthit <parthit@localhost.localdomain>
2017-09-17 20:22:38 +0530

    Half way done with the new feature, so far so good.

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+function newfeature() {
+
+console.log('Writing bohot sara lines of code');
+console.log('Halfway done with this pseudo code that ive been writing');
+};
+
+
+
+
+
+
+
 function something (){
 
 	console.log('I\'m something inn the something function');
@@ -6,3 +18,5 @@ function something (){
 console.log('hello world');
 something();
 console.log('trying it on myself');
+
+newfeature();
