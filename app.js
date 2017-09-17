d265d0ae44c60705731cd5c59c002389e2e0fef7
Parthit <parthit@localhost.localdomain>
2017-09-17 20:38:50 +0530

    new-feature was completed

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ function newfeature() {
 
 console.log('Writing bohot sara lines of code');
 console.log('Halfway done with this pseudo code that ive been writing');
+console.log('Finally done with this bug fixing time pass');
+
 };
 
 
